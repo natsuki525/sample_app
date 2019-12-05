e37dc3ac167e326d85ce4d6b32096990693f4ee9
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2019-12-05 16:20:41 +0900

    [Add] 画像投稿機能追加

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -9,17 +9,31 @@ class TodolistsController < ApplicationController
   	list =List.new(list_params)
   	# DBへ保存する
   	list. save
-  	# トップ画面へリダイレクト
-  	redirect_to '/top'
+  	# 詳細画面へリダイレクト
+  	redirect_to todolist_path(list.id)
   end
 
   def index
   	@lists = List.all
   end
 
+  def show
+  	@list = List.find(params[:id])
+  end
+
+  def edit
+  	@list = List.find(params[:id])
+  end
+
+  def update
+  	list = List.find(params[:id])
+  	list.update(list_params)
+  	redirect_to todolist_path(list.id)
+  end
+
   private
 
    def list_params
-   		params.require(:list).permit(:title, :body)
+   		params.require(:list).permit(:title, :body, :image)
    end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '37a4fddb8aac0711b431409f00116f4dcbcca599e7edc0c67edbc1b02f04e2571dd0988971e1ae5e5f42e28866d628649ec017e89a90b3f5fda3c5b613f1b4b2'
