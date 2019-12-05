43c2861b41f26e113043568a5837332c9b3bf25e
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2019-12-05 12:55:57 +0900

    [Add] createアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get 'todolists/new'
 	get 'top' => 'root#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top' => 'root#top'
+
+  post 'todolists' => 'todolists#create'
 end
